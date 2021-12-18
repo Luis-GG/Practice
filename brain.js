f9348c2df37e84c44d6fe2da160e9d4e3df22a9d
luis <gero.garc97@gmail.com>
2021-12-18 16:33:27 -0600

    basic header and nav bar

--- brain.js
+++ brain.js
@@ -1,1 +1,16 @@
-console.log(fetch('https://api.rocketreach.co/v2/api/lookupProfile'));
+const headerDiv = document.querySelector(".header-cont");
+console.log(headerDiv)
+
+
+function ranRGB() {
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+    return `rgb(${R},${G},${B})`
+}
+
+
+setInterval(function () {
+    headerDiv.style.backgroundColor = ranRGB();
+}, 1000);
+
